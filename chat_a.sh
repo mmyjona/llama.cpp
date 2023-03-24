edd6d46bbbe225649fd999c8bf4fe1263e2d8726
mmyjona <jonathan.gonse@gmail.com>
2023-03-24 11:54:50 +0800

    fix alpaca

--- chat_a.sh
+++ chat_a.sh
@@ -1,1 +1,1 @@
-./build/llama.exe -m ./models/Alpaca/13b/ggml-alpaca-13b-q4.bin --color -f ./prompts/alpaca.txt -ins --repeat_last_n 256 --repeat_penalty 1.178 --temp 0.7 --top_k 40 --top_p 0 --color -i -r "User:"
+./build/bin/main.exe -m ./models/Alpaca/13b/ggml-alpaca-13b-q4.bin --color -f ./prompts/alpaca.txt -ins --repeat_last_n 256 --repeat_penalty 1.178 --temp 0.7 --top_k 40 --top_p 0 --color -r "User:" --n_parts 1
