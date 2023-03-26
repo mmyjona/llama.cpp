f4bfa6a57a6898a4262b43b1a7c9de5107da86b2
mmyjona <jonathan.gonse@gmail.com>
2023-03-27 00:08:21 +0800

    cus

--- sum.sh
+++ sum.sh
@@ -8,7 +8,6 @@ else
   echo "plz provide text file path for summarize"
   exit 0
 fi
-./build/bin/main.exe -m ./models/Alpaca/$model/ggml-alpaca-7b-q4.bin --color -f ./prompts/alpaca.txt\
- --repeat_last_n 256 --color --n_parts 1\
- -p "Building a website can be done in 10 simple steps:"
-#  -p "[Text]: $(cat $text_file)\n [Summary]:"
+./build/bin/main.exe -m ./models/Alpaca/$model/ggml-alpaca-7b-q4.bin --color -f ./prompts/alpaca_sum.txt\
+ --n_parts 1 -c 2048\
+ -p "'text: $(cat $text_file)'\n summary:"
